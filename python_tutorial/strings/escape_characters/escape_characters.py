4aea24a63a22ce925c253252432a8503e8f8f895
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-25 15:02:12 +0100

    Create escape characters file

---
+++ python_tutorial/strings/escape_characters/escape_characters.py
@@ -0,0 +1,16 @@
+txt = "We are the so-called \"Vikings\" from the north."
+
+print(txt)
+
+# Escape Characters
+
+# Code 	Result 	
+# \' 	Single Quote 	
+# \\ 	Backslash 	
+# \n 	New Line 	
+# \r 	Carriage Return 	
+# \t 	Tab 	
+# \b 	Backspace 	
+# \f 	Form Feed 	
+# \ooo 	Octal value 	
+# \xhh 	Hex value
