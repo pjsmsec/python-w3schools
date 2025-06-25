a7baa7de114609ab4dc1a4ddba1bf73b0377af1c
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-25 11:36:20 +0100

    Create quotes file

---
+++ python_tutorial/variables/variables/quotes.py
@@ -0,0 +1,13 @@
+x = "John"
+y = 'John'
+
+if x == y:
+    print("The strings are the same")
+
+# Variable names are case-sensitive
+a = 4
+A = "Sally"
+# A will not overwrite a
+
+print(a)
+print(A)
