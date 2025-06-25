98a1c88d324a0255e8b9b847cd8979c64dce6e7a
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-25 11:25:31 +0100

    Create comments file

---
+++ python_tutorial/comments/comments.py
@@ -0,0 +1,22 @@
+# This is a comment
+print("Hello, World!")
+
+print()
+
+print("Hello, World!")  # This is a comment
+
+print()
+
+# print("Hello, World!")
+print("Cheers, Mate!")
+
+print()
+
+# Multiline Comments
+# String literals without an atribution can be used as comments
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
