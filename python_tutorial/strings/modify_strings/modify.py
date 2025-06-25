f0fee9fe66c70574e83da94704361a0a1186b386
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-25 14:34:26 +0100

    Create modifying strings file

---
+++ python_tutorial/strings/modify_strings/modify.py
@@ -0,0 +1,25 @@
+a = "Hello, World!"
+
+print(a.upper())
+
+print()
+
+print(a.lower())
+
+print()
+
+# Remove whitespaces
+b = "Hello, World!    "
+print(b.strip())
+
+print()
+
+# Replace string
+print(a.replace("H", "J"))
+# Does not alter the original string
+
+print()
+
+# Split string
+print(a.split(","))
+# Does not alter the original string
