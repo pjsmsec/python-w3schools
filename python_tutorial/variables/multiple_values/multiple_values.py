a3e86387193ad5caef95c832b8bf9a1f14ef57b5
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-25 11:44:55 +0100

    Create multiple value attribution file

---
+++ python_tutorial/variables/multiple_values/multiple_values.py
@@ -0,0 +1,25 @@
+x, y , z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+print()
+
+# One value to multiple variables
+x = y = z = "Orange"
+
+print(x)
+print(y)
+print(z)
+
+# Unpack a collection
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+
+print()
+
+print(x)
+print(y)
+print(z)
