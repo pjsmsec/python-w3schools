e5ab99ae6da1ccd394b7b0e67c7fb2cd7a6dd7c3
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-25 14:25:20 +0100

    Create string slicing file

---
+++ python_tutorial/strings/slicing_strings/slicing.py
@@ -0,0 +1,20 @@
+b = "Hello, World!"
+print(b[2:5])
+# Arrays are 0 based
+# The first value is inclusive and the last is exclusive
+
+print()
+
+# Slice from the start
+print(b[:5])
+
+print()
+
+# Slice to the end
+print(b[2:])
+
+print()
+
+# Negative indexing
+print(b[-5:-2])
+# The first value is inclusive and the last is exclusive
