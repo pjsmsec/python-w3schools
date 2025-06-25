cdfc51b8d62529b8e2cce759b169aaa831d34c42
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-25 14:55:19 +0100

    Create string formatting file

---
+++ python_tutorial/strings/format_strings/format_strings.py
@@ -0,0 +1,27 @@
+# F-strings
+age = 36
+txt = f"My name is John, I am {age}"
+
+print(txt)
+
+print()
+
+# Placeholders and modifiers
+price = 59
+txt = f"The price is {price} dollars"
+
+print(txt)
+
+print()
+
+# Modifiers
+txt = f"The price is {price:.2f} dollars"
+
+print(txt)
+
+print()
+
+# A placeholder can contain code
+txt = f"The price is {20 * 59} dollars"
+
+print(txt)
