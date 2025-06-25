ad4d6cb5c9c5447ac5ed01f0554e5ea9751eb5d1
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-25 13:34:18 +0100

    Create number type conversion file

---
+++ python_tutorial/numbers/type_conversion.py
@@ -0,0 +1,20 @@
+x = 1
+y = 2.8
+z = 1j
+
+# convert form int to float
+a = float(x)
+
+# convert from float to int
+b = int(y)
+
+# convert from gloat to complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
