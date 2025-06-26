14036a0d2a3524a1a28e5144a12fbbba53c1c72e
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-26 11:38:39 +0100

    Create operators file

---
+++ python_tutorial/operators/operators.py
@@ -0,0 +1,24 @@
+print(10 + 5)
+
+# Python Arithmetic Operators
+
+# Operator 	Name 	        Example 	
+# + 	    Addition 	    x + y 	
+# - 	    Subtraction 	x - y 	
+# * 	    Multiplication 	x * y 	
+# / 	    Division 	    x / y 	
+# % 	    Modulus 	    x % y 	
+# ** 	    Exponentiation 	x ** y 	
+# // 	    Floor division  x // y
+
+print()
+
+print((6 + 3) - (6 + 3))
+
+print()
+
+print(100 + 5 * 3)
+
+print()
+
+print(5 + 4 - 7 + 3)
