77696b8bb556375c4e350bfecb36173ee62e3af3
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-28 12:02:53 +0100

    Create add set items file

---
+++ python_tutorial/sets/add-set-items/add-set-items.py
@@ -0,0 +1,16 @@
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+print()
+
+# Add any iterable
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
