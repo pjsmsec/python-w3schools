130d496cff3306a390a9c44ff6c449323e7d4214
pjsmelodev <pjsm.lesi@gmail.com>
2025-06-28 11:57:32 +0100

    Create set item accessing file

---
+++ python_tutorial/sets/access-items/access-items.py
@@ -0,0 +1,14 @@
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+    print(x)
+
+print()
+
+# Check if "banana" is present in the set:
+print("banana" in thisset)
+
+print()
+
+# Check if "banana" is NOT present in the set:
+print("banana" not in thisset)
