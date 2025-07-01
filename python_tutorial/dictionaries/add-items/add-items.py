d9f05233a20a55b3cf8d6000c1804ca27a113257
pjsmelodev <pjsm.lesi@gmail.com>
2025-07-01 11:32:32 +0100

    Create update dictionary file

---
+++ python_tutorial/dictionaries/add-items/add-items.py
@@ -0,0 +1,15 @@
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+thisdict["color"] = "red"
+
+print(thisdict)
+
+print()
+
+thisdict.update({"color": "red"})
+
+print(thisdict)
