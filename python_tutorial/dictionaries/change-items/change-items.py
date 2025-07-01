9cc9b80c5725420845c7dc9115132791df6dde18
pjsmelodev <pjsm.lesi@gmail.com>
2025-07-01 11:26:03 +0100

    Create change dictionary items file

---
+++ python_tutorial/dictionaries/change-items/change-items.py
@@ -0,0 +1,13 @@
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+thisdict["year"] = 2018
+
+# The update() method will update the dictionary with the items from the given 
+# argument.
+thisdict.update({"year": 2020})
+
+print(thisdict)
