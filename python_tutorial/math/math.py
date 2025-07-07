6e743a12c026ea386b5e2c6e04eb721dc24c0f1b
pjsmelodev <pjsm.lesi@gmail.com>
2025-07-07 15:49:28 +0100

    Create math file

---
+++ python_tutorial/math/math.py
@@ -0,0 +1,38 @@
+# Built-in math functions
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+print()
+
+x = abs(-7.25)
+print(x)
+
+print()
+
+x = pow(4, 3)
+print(x)
+
+# The math module
+import math
+
+x = math.sqrt(64)
+print(x)
+
+print()
+
+# The math.ceil() method rounds a number upwards to it's nearest integer.
+# The math.floor() method rounds a number downwards to it's nearest integer.
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x)
+print(y)
+
+print()
+
+x = math.pi
+
+print(x)
