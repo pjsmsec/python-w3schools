9a40eb5603b41f800f6150647ced7fb6de23d33b
pjsmelodev <pjsm.lesi@gmail.com>
2025-07-08 19:44:40 +0100

    Create trees file

---
+++ python-dsa/trees/trees.py
@@ -0,0 +1,14 @@
+# Trees
+
+# A tree is a hierarchical data structure consisting of nodes connected by edges
+# Each node contains a value and references to it's child node
+# A single element can have multiple 'next' elements, allowing the data structure 
+# to branch out in various directions
+
+# Types of trees:
+# Binary Trees - Each node has up to two children
+# Binary Search Trees (BSTs) - A type of Binary Tree where for each node, the left 
+# child node has a lower value, and the right child value has a higher value
+# AVL Trees - A type of Binary Search Tree that self-balances so that for every 
+# node, the difference in height between the left and right subtrees is at most one.
+# This balance is maintained through rotations when nodes are inserted or deleted.
