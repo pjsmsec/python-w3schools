09b1bac4a4356177f79d6c9038b61d0240a43d44
pjsmelodev <pjsm.lesi@gmail.com>
2025-07-08 17:26:34 +0100

    Create lists and arrays file

---
+++ python-dsa/lists-arrays/lists-arrays.py
@@ -0,0 +1,34 @@
+# Lists
+
+# Creating lists
+x = []
+
+y = [1, 2, 3, 4, 5]
+
+z = [1, "hello", 3.14, True]
+
+# List Methods
+
+# Example: append one element to the list, and sort the list ascending:
+x = [9, 12, 7, 4, 11]
+
+x.append(8)
+
+x.sort()
+
+print(x)
+
+print()
+
+# Create algorithms
+my_array = [7, 12, 9, 4, 11, 8]
+minVal = my_array[0]
+
+for i in my_array:
+    if i < minVal:
+        minVal = 1
+
+print('Lowest value:', minVal)
+
+print()
+
