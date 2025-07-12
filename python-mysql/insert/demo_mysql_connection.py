49906fd5f4138c3b22331233578efd03e1d43d15
pjsmelodev <pjsm.lesi@gmail.com>
2025-07-12 15:03:36 +0100

    Create insert into table file

---
+++ python-mysql/insert/demo_mysql_connection.py
@@ -0,0 +1,45 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="teste",
+    password="",
+    database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+# Insert into table
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
+# Note: notice the statement: mydb.commit().
+# It is required to make the changes.
+
+# Insert multiple rows
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = [
+    ('Peter', 'Lowstreet 4'),
+    ('Amy', 'Apple st 652'),
+    ('Hannah', 'Mountain 21'),
+    ('Michael', 'Mountain 21'),
+    ('Sandy', 'Ocean blvd 2'),
+    ('Betty', 'Green Grass 1'),
+    ('Richard', 'Sky st 331'),
+    ('Susan', 'One way 98'),
+    ('Vicky', 'Yellow Garden 2'),
+    ('Ben', 'Park Lane 38'),
+    ('William', 'Central st 954'),
+    ('Chuck', 'Main Road 989'),
+    ('Viola', 'Sideway 1633')
+]
+
+mycursor.executemany(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "was inserted.")
