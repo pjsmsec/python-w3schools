92a42fdb1df2e835b8e4a7dd68e7d015828d7123
pjsmelodev <pjsm.lesi@gmail.com>
2025-07-15 16:52:01 +0100

    Create example file

---
+++ examples/examples/python-mysql/prevent-injection.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="myusername",
+  password="mypassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers WHERE address = %s"
+adr = ("Yellow Garden 2", )
+
+mycursor.execute(sql, adr)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
